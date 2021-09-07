f4438c135393a531d220786d7501dc975eb6fbc6
Filipe Laíns <lains@riseup.net>
2021-09-07 17:58:17 +0100

    tests: pep517: add test for requires_for_build_wheel
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/packages/library-pep621/example.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int sum(int, int);
+
+int main() {
+	printf("sum: %d", sum(1, 2));
+}
--- tests/test_pep517.py
+++ tests/test_pep517.py
@@ -17,3 +17,17 @@ from .conftest import cd_package
 def test_get_requires_for_build_sdist(package, expected):
     with cd_package(package):
         assert set(mesonpy.get_requires_for_build_sdist()) == expected
+
+
+@pytest.mark.parametrize(
+    ('package', 'expected'),
+    [
+        ('pure', set()),  # pure and no PEP 621
+        ('full-metadata', {'pep621 >= 0.2.0'}),  # pure and PEP 621
+        ('library', {'auditwheel >= 4.0.0'}),  # not pure and not PEP 621
+        ('library-pep621', {'auditwheel >= 4.0.0', 'pep621 >= 0.2.0'}),  # not pure and PEP 621
+    ]
+)
+def test_get_requires_for_build_wheel(package, expected):
+    with cd_package(package):
+        assert set(mesonpy.get_requires_for_build_wheel()) == expected | {'wheel >= 0.36.0', 'ninja'}
