d1ed639a4df1bd5b2cb35bad46413881eb5469c4
Filipe Laíns <lains@riseup.net>
2021-09-07 18:28:22 +0100

    tests: project: add test for name and version
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,6 +4,32 @@ import pytest
 
 import mesonpy
 
+from .conftest import cd_package
+
+
+@pytest.mark.parametrize(
+    ('package'),
+    [
+        'library',
+        'library-pep621',
+    ]
+)
+def test_name(package):
+    with cd_package(package), mesonpy.Project.with_temp_working_dir() as project:
+        assert project.name == package
+
+
+@pytest.mark.parametrize(
+    ('package'),
+    [
+        'library',
+        'library-pep621',
+    ]
+)
+def test_version(package):
+    with cd_package(package), mesonpy.Project.with_temp_working_dir() as project:
+        assert project.version == '1.0.0'
+
 
 def test_unsupported_dynamic(package_unsupported_dynamic):
     with pytest.raises(mesonpy.MesonBuilderError, match='Unsupported dynamic fields: dependencies'):
