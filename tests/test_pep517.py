42468e8ef0731fd8e029beda96cf6bdc6a8f0e5a
Filipe Laíns <lains@riseup.net>
2021-09-07 17:57:25 +0100

    tests: pep517: refactor to use pytest.mark.parametrize
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_pep517.py
+++ tests/test_pep517.py
@@ -1,11 +1,19 @@
 # SPDX-License-Identifier: EUPL-1.2
 
+import pytest
+
 import mesonpy
 
-
-def test_get_requires_for_build_sdist_no_pep621(package_pure):
-    assert mesonpy.get_requires_for_build_sdist() == []
+from .conftest import cd_package
 
 
-def test_get_requires_for_build_sdist_pep621(package_full_metadata):
-    assert mesonpy.get_requires_for_build_sdist() == ['pep621 >= 0.2.0']
+@pytest.mark.parametrize(
+    ('package', 'expected'),
+    [
+        ('pure', set()),  # no PEP 621
+        ('full-metadata', {'pep621 >= 0.2.0'}),  # PEP 621
+    ]
+)
+def test_get_requires_for_build_sdist(package, expected):
+    with cd_package(package):
+        assert set(mesonpy.get_requires_for_build_sdist()) == expected
