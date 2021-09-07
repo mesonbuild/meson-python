ee4019dbee0fa13cf4d7ecbdfa03481dd72cf2c2
Filipe Laíns <lains@riseup.net>
2021-09-07 14:01:35 +0100

    test: wheel: ignore order
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -12,35 +12,35 @@ EXT_SUFFIX = sysconfig.get_config_var('EXT_SUFFIX')
 def test_contents(package_library, wheel_library):
     artifact = wheel.wheelfile.WheelFile(wheel_library)
 
-    for name, regex in zip(artifact.namelist(), [
+    print(sorted(artifact.namelist()))
+    for name, regex in zip(sorted(artifact.namelist()), [
+        re.escape('library-1.0.0.data/scripts/example'),
+        re.escape('library-1.0.0.dist-info/METADATA'),
+        re.escape('library-1.0.0.dist-info/RECORD'),
+        re.escape('library-1.0.0.dist-info/WHEEL'),
         r'library\.libs/libexample.*\.so',
-    ] + list(map(re.escape, [
-        'library-1.0.0.data/scripts/example',
-        'library-1.0.0.dist-info/RECORD',
-        'library-1.0.0.dist-info/WHEEL',
-        'library-1.0.0.dist-info/METADATA',
-    ]))):
+    ]):
         assert re.match(regex, name), f'`{name}` does not match `{regex}`'
 
 
 def test_purelib_and_platlib(wheel_purelib_and_platlib):
     artifact = wheel.wheelfile.WheelFile(wheel_purelib_and_platlib)
 
-    assert artifact.namelist() == [
-        'purelib_and_platlib-1.0.0.dist-info/METADATA',
-        'purelib_and_platlib-1.0.0.dist-info/WHEEL',
+    assert sorted(artifact.namelist()) == [
         'pure.py',
         f'purelib_and_platlib-1.0.0.data/platlib/plat{EXT_SUFFIX}',
+        'purelib_and_platlib-1.0.0.dist-info/METADATA',
         'purelib_and_platlib-1.0.0.dist-info/RECORD',
+        'purelib_and_platlib-1.0.0.dist-info/WHEEL',
     ]
 
 
 def test_pure(wheel_pure):
     artifact = wheel.wheelfile.WheelFile(wheel_pure)
 
-    assert artifact.namelist() == [
+    assert sorted(artifact.namelist()) == [
         'pure-1.0.0.dist-info/METADATA',
+        'pure-1.0.0.dist-info/RECORD',
         'pure-1.0.0.dist-info/WHEEL',
         'pure.py',
-        'pure-1.0.0.dist-info/RECORD',
     ]
