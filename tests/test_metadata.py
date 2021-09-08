5ba9194a8e1e365ee60d03d1d1924589744b3c58
Filipe Laíns <lains@riseup.net>
2021-09-08 12:35:19 +0100

    tests: metadata: add test for PEP 621 metadata
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -12,3 +12,38 @@ def test_no_pep621(sdist_library):
         Name: library
         Version: 1.0.0
     ''').strip()
+
+
+def test_pep621(sdist_full_metadata):
+    sdist = tarfile.open(sdist_full_metadata, 'r:gz')
+
+    assert sdist.extractfile('full_metadata-1.2.3/PKG-INFO').read().decode() == textwrap.dedent('''\
+        Metadata-Version: 2.1
+        Name: full_metadata
+        Version: 1.2.3
+        Summary: Some package with all of the PEP 621 metadata
+        Keywords: full metadata
+        Home-page: https://example.com
+        Author: Unknown <jhon.doe@example.com>, Jane Doe
+        Author-Email: Unknown <jhon.doe@example.com>, Jane Doe
+        Maintainer: Jane Doe <jane.doe@example.com>
+        Maintainer-Email: Jane Doe <jane.doe@example.com>
+        Classifier: Development Status :: 4 - Beta
+        Classifier: Programming Language :: Python
+        Project-URL: Homepage, https://example.com
+        Project-URL: Documentation, https://readthedocs.org
+        Project-URL: Repository, https://github.com/FFY00/mesonpy
+        Project-URL: Changelog, https://github.com/FFY00/mesonpy/blob/master/CHANGELOG.md
+        Requires-Python: >=3.10
+        Requires-Dist: a
+        Requires-Dist: b > 1
+        Requires-Dist: c > 2; os_name != "nt"
+        Requires-Dist: d<3; extra == "test"
+        Requires-Dist: e[all]; extra == "test"
+        Provides-Extra: test
+        Description-Content-Type: text/markdown
+
+        # full-metadata
+
+        An example package with all of the PEP 621 metadata!
+    ''')
