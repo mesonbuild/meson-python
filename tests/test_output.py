b158c4ee7a2abc721ada008b4ee3a7e671b66050
Filipe Laíns <lains@riseup.net>
2021-09-08 12:45:38 +0100

    tests: output: add tests for colors
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/test_output.py
@@ -0,0 +1,50 @@
+# SPDX-License-Identifier: EUPL-1.2
+
+import importlib
+
+import pytest
+
+import mesonpy
+
+
+@pytest.fixture()
+def reload_module():
+    try:
+        yield
+    finally:
+        importlib.reload(mesonpy)
+
+
+@pytest.mark.parametrize(
+    ('tty', 'env', 'colors'),
+    [
+        (False, {}, False),
+        (True, {}, True),
+        (False, {'NO_COLOR': ''}, False),
+        (True, {'NO_COLOR': ''}, False),
+        (False, {'FORCE_COLOR': ''}, True),
+        (True, {'FORCE_COLOR': ''}, True),
+    ],
+)
+def test_colors(mocker, monkeypatch, reload_module, tty, env, colors):
+    mocker.patch('sys.stdout.isatty', return_value=tty)
+    for key, value in env.items():
+        monkeypatch.setenv(key, value)
+
+    importlib.reload(mesonpy)  # reload module to set _STYLES
+
+    assert mesonpy._STYLES == (mesonpy._COLORS if colors else mesonpy._NO_COLORS)
+
+
+def test_colors_conflict(monkeypatch, reload_module):
+    with monkeypatch.context() as m:
+        m.setenv('NO_COLOR', '')
+        m.setenv('FORCE_COLOR', '')
+
+        with pytest.warns(
+            UserWarning,
+            match='Both NO_COLOR and FORCE_COLOR environment variables are set, disabling color',
+        ):
+            importlib.reload(mesonpy)
+
+        assert mesonpy._STYLES == mesonpy._NO_COLORS
