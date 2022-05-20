c5640592df2c06d76718d4821239a65eec6895de
Filipe Laíns <lains@riseup.net>
2022-05-20 19:23:34 +0100

    mesonpy: include uncommited changes in sdist
    Fixes #53
    
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_sdist.py
+++ tests/test_sdist.py
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: EUPL-1.2
 
+import os
 import tarfile
+import textwrap
+
+import mesonpy
+
+from .conftest import in_git_repo_context
 
 
 def test_contents(sdist_library):
@@ -14,3 +20,48 @@ def test_contents(sdist_library):
         'library-1.0.0/pyproject.toml',
         'library-1.0.0/PKG-INFO',
     }
+
+
+def test_contents_subdirs(sdist_subdirs):
+    sdist = tarfile.open(sdist_subdirs, 'r:gz')
+
+    assert set(sdist.getnames()) == {
+        'subdirs-1.0.0/PKG-INFO',
+        'subdirs-1.0.0/meson.build',
+        'subdirs-1.0.0/pyproject.toml',
+        'subdirs-1.0.0/subdirs/__init__.py',
+        'subdirs-1.0.0/subdirs/a/__init__.py',
+        'subdirs-1.0.0/subdirs/a/b/c.py',
+        'subdirs-1.0.0/subdirs/b/c.py',
+    }
+
+
+def test_contents_unstaged(package_pure, tmpdir):
+    new_data = textwrap.dedent('''
+    def bar():
+        return 'foo'
+    ''').strip()
+
+    with open('pure.py', 'r') as f:
+        old_data = f.read()
+
+    try:
+        with in_git_repo_context(), open('pure.py', 'w') as f, open('crap', 'x'):
+            f.write(new_data)
+
+        sdist_path = mesonpy.build_sdist(os.fspath(tmpdir))
+    finally:
+        with open('pure.py', 'w') as f:
+            f.write(old_data)
+        os.unlink('crap')
+
+    sdist = tarfile.open(tmpdir / sdist_path, 'r:gz')
+
+    assert set(sdist.getnames()) == {
+        'pure-1.0.0/PKG-INFO',
+        'pure-1.0.0/meson.build',
+        'pure-1.0.0/pure.py',
+        'pure-1.0.0/pyproject.toml',
+    }
+    read_data = sdist.extractfile('pure-1.0.0/pure.py').read().replace(b'\r\n', b'\n')
+    assert read_data == new_data.encode()
