a394f9c6ba40748822587a3c0d91aa418c410e78
Filipe Laíns <lains@riseup.net>
2022-06-10 00:48:06 +0100

    mesonpy: error out if we are running on an unsupported interpreter
    Fixes #69
    
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2
 
+import platform
+
 import pytest
 
 import mesonpy
@@ -33,5 +35,14 @@ def test_version(package):
 
 def test_unsupported_dynamic(package_unsupported_dynamic):
     with pytest.raises(mesonpy.MesonBuilderError, match='Unsupported dynamic fields: dependencies'):
+        with mesonpy.Project.with_temp_working_dir():
+            pass
+
+
+def test_unsupported_python_version(package_unsupported_python_version):
+    with pytest.raises(mesonpy.MesonBuilderError, match=(
+        f'Unsupported Python version `{platform.python_version()}`, '
+        'expected `==1.0.0`'
+    )):
         with mesonpy.Project.with_temp_working_dir():
             pass
