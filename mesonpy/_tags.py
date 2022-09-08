75b3cebf4104a9e373c260a2b49f65f76ad6bf3c
Filipe Laíns <lains@riseup.net>
2022-09-08 12:01:18 +0100

    BUG: handle PyPy tags correctly
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- mesonpy/_tags.py
+++ mesonpy/_tags.py
@@ -4,8 +4,6 @@
 
 import abc
 import re
-import sys
-import warnings
 
 from typing import Any, Optional
 
@@ -61,7 +59,7 @@ class StableABITag(Tag):
         return hash(str(self))
 
 
-class LinuxInterpreterTag(Tag):
+class InterpreterTag(Tag):
     def __init__(self, value: str) -> None:
         parts = value.split('-')
         if len(parts) < 2:
@@ -74,7 +72,7 @@ class LinuxInterpreterTag(Tag):
         self._interpreter_version = parts[1]
         self._additional_information = parts[2:]
 
-        if self.implementation not in ('cpython', 'pypy', 'pypy3'):
+        if self.implementation != 'cpython' and not self.implementation.startswith('pypy'):
             raise NotImplementedError(
                 f'Unknown Python implementation: {self.implementation}. '
                 'Please report this to https://github.com/FFY00/mesonpy/issues '
@@ -105,21 +103,18 @@ class LinuxInterpreterTag(Tag):
         if self.implementation == 'cpython':
             # The Python tag for CPython does not seem to include the flags suffixes.
             return f'cp{self.interpreter_version}'.rstrip('dmu')
-        elif self.implementation in ('pypy', 'pypy3'):
-            interpreter_version = f'{sys.version_info[0]}{sys.version_info[1]}'
-            return f'pp{interpreter_version}'
+        elif self.implementation.startswith('pypy'):
+            return f'pp{self.implementation[4:]}'
+            # XXX: FYI older PyPy version use the following model
+            #      pp{self.implementation[4]}{self.interpreter_version[2:]}
         raise ValueError(f'Unknown implementation: {self.implementation}')
 
     @property
     def abi(self) -> str:
-        # XXX: This is a bit flimsy and needs custom logic to support each case,
-        #      but currently there's no better way to do it.
         if self.implementation == 'cpython':
             return f'cp{self.interpreter_version}'
-        elif self.implementation == 'pypy':
-            return f'pypy_{self.interpreter_version}'
-        elif self.implementation == 'pypy3':
-            return f'pypy3_{self.interpreter_version}'
+        elif self.implementation.startswith('pypy'):
+            return f'{self.implementation}_{self.interpreter_version}'
         raise ValueError(f'Unknown implementation: {self.implementation}')
 
     def __eq__(self, other: Any) -> bool:
@@ -128,40 +123,6 @@ class LinuxInterpreterTag(Tag):
             and other.implementation == self.implementation
             and other.interpreter_version == self.interpreter_version
         )
-
-    def __hash__(self) -> int:
-        return hash(str(self))
-
-
-class WindowsInterpreterTag(Tag):
-    def __init__(self, value: str) -> None:
-        # XXX: This is not actually standardized, so our implementation relies
-        #      on observing how the current software behaves!
-        self._parts = value.split('-')
-        if len(self.parts) != 2:
-            warnings.warn(
-                'Unexpected native module tag name, the ABI dectection might be broken. '
-                'Please report this to https://github.com/FFY00/mesonpy/issues '
-                'and include information about the Python distribution you are using.'
-            )
-
-    @property
-    def parts(self) -> Sequence[str]:
-        return tuple(self._parts)
-
-    def __str__(self) -> str:
-        return '-'.join(self.parts)
-
-    @property
-    def python(self) -> str:
-        return self.abi
-
-    @property
-    def abi(self) -> str:
-        return self._parts[0]
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, self.__class__) and other.parts == self.parts
 
     def __hash__(self) -> int:
         return hash(str(self))
