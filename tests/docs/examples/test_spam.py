02cd33a49c6b75cade6ffc575dbad405214b2fd3
pdmurray <peynmurray@gmail.com>
2022-09-20 14:22:19 -0700

    Skip test_spam if not running linux, or if running pypy-3.8
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/docs/examples/test_spam.py
+++ tests/docs/examples/test_spam.py
@@ -1,3 +1,4 @@
+import platform
 import subprocess
 import sys
 
@@ -6,7 +7,14 @@ import pytest
 from .conftest import build_project_wheel, examples_dir
 
 
+# This test fails on Ubuntu and MacOS for pypy-3.8;
+# see https://github.com/FFY00/meson-python/pull/136 for more information.
+@pytest.mark.skipif(platform.system() != 'Linux', reason='Unsupported on this platform for now')
 @pytest.mark.skipif(sys.version_info < (3, 8), reason='Example only supports >=3.8')
+@pytest.mark.skipif(
+    sys.version_info == (3, 8) and platform.python_implementation() == 'PyPy',
+    reason='Unsupported on PyPy 3.8.'
+)
 def test_build_and_import(virtual_env, tmp_dir_session):
     """Test that the wheel for the spam example builds, installs, and imports."""
     wheel = build_project_wheel(
