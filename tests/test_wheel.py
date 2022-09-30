25b8d286b68952f6ddb5bfcfc596c5002e852ae4
Filipe Laíns <lains@riseup.net>
2022-09-30 15:26:34 +0100

    BUG: ignore macOS minor version
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -51,7 +51,9 @@ else:
 platform_ = sysconfig.get_platform()
 if platform.system() == 'Darwin':
     parts = platform_.split('-')
-    parts[1] = platform.mac_ver()[0]
+    parts[1] = platform.mac_ver()[0].split('.')[0]
+    if parts[1] in ('11', '12'):
+        parts[1] += '.0'
     platform_ = '-'.join(parts)
 PLATFORM_TAG = platform_.replace('-', '_').replace('.', '_')
 
