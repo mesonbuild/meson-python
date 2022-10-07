4bdb1d2d38c5810c23f32f8e59bda58796246391
Filipe Laíns <lains@riseup.net>
2022-10-07 05:24:23 +0100

    ENH: allow users to pass options directly to Meson
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: MIT
 
+import contextlib
 import platform
+import subprocess
+import sys
 
 import pytest
 
@@ -46,3 +49,43 @@ def test_unsupported_python_version(package_unsupported_python_version):
     )):
         with mesonpy.Project.with_temp_working_dir():
             pass
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 8),
+    reason="unittest.mock doesn't support the required APIs for this test",
+)
+def test_user_args(package_user_args, mocker, tmp_dir_session):
+    mocker.patch('mesonpy.Project._meson')
+
+    def last_two_meson_args():
+        return [
+            call.args[-2:] for call in mesonpy.Project._meson.call_args_list
+        ]
+
+    # create the build directory ourselves because Project._meson is mocked
+    builddir = str(tmp_dir_session / 'build')
+    subprocess.check_call(['meson', 'setup', '.', builddir])
+
+    config_settings = {
+        'builddir': builddir,  # use the build directory we created
+        'dist_args': ('cli-dist',),
+        'setup_args': ('cli-setup',),
+        'compile_args': ('cli-compile',),
+        'install_args': ('cli-install',),
+    }
+
+    with contextlib.suppress(Exception):
+        mesonpy.build_sdist(tmp_dir_session / 'dist', config_settings)
+    with contextlib.suppress(Exception):
+        mesonpy.build_wheel(tmp_dir_session / 'dist', config_settings)
+
+    assert last_two_meson_args() == [
+        # sdist
+        ('config-setup', 'cli-setup'),
+        ('config-dist', 'cli-dist'),
+        # wheel
+        ('config-setup', 'cli-setup'),
+        ('config-compile', 'cli-compile'),
+        ('config-install', 'cli-install'),
+    ]
