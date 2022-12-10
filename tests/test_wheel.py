11c65aaf67a20f624f6c91209111d21bd4be83a8
Filipe Laíns <lains@riseup.net>
2022-12-10 04:08:13 +0000

    TST: add test for editable wheels
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -220,3 +220,22 @@ def test_entrypoints(wheel_full_metadata):
             [gui_scripts]
             example-gui = example:gui
         ''').strip()
+
+
+def test_editable(
+    package_imports_itself_during_build,
+    editable_imports_itself_during_build,
+    venv,
+):
+    venv.pip('install', os.fspath(editable_imports_itself_during_build))
+
+    assert venv.python('-c', 'import plat; print(plat.foo())').strip() == 'bar'
+
+    plat = package_imports_itself_during_build / 'plat.c'
+    plat_text = plat.read_text()
+    try:
+        plat.write_text(plat_text.replace('bar', 'something else'))
+
+        assert venv.python('-c', 'import plat; print(plat.foo())').strip() == 'something else'
+    finally:
+        plat.write_text(plat_text)
