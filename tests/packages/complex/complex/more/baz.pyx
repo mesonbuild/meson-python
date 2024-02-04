4924a5a3c21261e32cf7ffd88b77183e5fc5eb13
Daniele Nicolodi <daniele@grinta.net>
2024-02-04 18:51:38 +0100

    ENH: set __path__ for module loaded in editable installation
    Set the __path__ module attribute to a placeholder path.  Because how
    editable installs are implemented, this cannot correspond to the
    filesystem path for the package.  And add a sys.path_hook that
    recognizes this paths and returns a path loader that implements
    iter_modules(). This allows pkgutil.iter_packages() to work properly.
    
    Fixes #557.
    Fixes #568.

---
+++ tests/packages/complex/complex/more/baz.pyx
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023 The meson-python developers
+#
+# SPDX-License-Identifier: MIT
+
+def answer():
+    return 42
---
+++ tests/packages/complex/foo.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 The meson-python developers
+#
+# SPDX-License-Identifier: MIT
+
+def foo():
+    return True
