e2177e37feee11e34012e11fff1cd3434bc1112c
Loïc Estève <loic.esteve@ymail.com>
2024-02-29 07:07:35 +0100

    Tackle comments

--- mesonpy/_editable.py
+++ mesonpy/_editable.py
@@ -309,7 +309,7 @@ class MesonpyMetaFinder(importlib.abc.MetaPathFinder):
             if self._build_cmd[0] == 'meson':
                 # On Windows meson compile is used so to do a dry run you need
                 # --ninja-args
-                dry_run_build_cmd = self._build_cmd + ['--ninja-args="-n"']
+                dry_run_build_cmd = self._build_cmd + ['--ninja-args=-n']
             else:
                 dry_run_build_cmd = self._build_cmd + ['-n']
 
@@ -317,10 +317,7 @@ class MesonpyMetaFinder(importlib.abc.MetaPathFinder):
             if b'ninja: no work to do.' not in p.stdout and b'samu: nothing to do' not in p.stdout:
                 module_names = ' '.join(sorted(self._top_level_modules))
                 build_command = ' '.join(self._build_cmd)
-                info_msg = (
-                    f'meson-python: building {module_names} with {build_command}'
-                )
-                print(info_msg, flush=True)
+                print(f'meson-python: building {module_names} with {build_command!r}', flush=True)
                 subprocess.run(self._build_cmd, cwd=self._build_path, env=env)
         else:
             subprocess.run(self._build_cmd, cwd=self._build_path, env=env, stdout=subprocess.DEVNULL)
