391d021d48a8f5dba6c79172b5b8a5e9427ffde2
Daniele Nicolodi <daniele@grinta.net>
2024-10-15 19:19:05 +0200

    BUG: allow install_subdir() of missing directories (#676)
    Remove the assert that made sure the source path for install_subdir()
    is an existing directory. Meson warns in this case but allows it.
    Empty directories for missing source directories are not created in
    the Python wheel as these are hardly meaningful.

--- mesonpy/__init__.py
+++ mesonpy/__init__.py
@@ -119,7 +119,6 @@ def _map_to_wheel(sources: Dict[str, Dict[str, Any]]) -> DefaultDict[str, List[T
                         f'It is recommended to set it in "import(\'python\').find_installation()"')
 
             if key == 'install_subdirs' or key == 'targets' and os.path.isdir(src):
-                assert os.path.isdir(src)
                 exclude_files = {os.path.normpath(x) for x in target.get('exclude_files', [])}
                 exclude_dirs = {os.path.normpath(x) for x in target.get('exclude_dirs', [])}
                 for root, dirnames, filenames in os.walk(src):
