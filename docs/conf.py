696b87bc341e7c9c72cf345414964a835f987d85
Daniele Nicolodi <daniele@grinta.net>
2024-10-19 18:57:19 +0200

    DOC: cleanup Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -2,108 +2,34 @@
 #
 # SPDX-License-Identifier: MIT
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
 import datetime
 import os
 import time
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 import mesonpy
 
-build_date = datetime.datetime.fromtimestamp(
-    int(os.environ.get('SOURCE_DATE_EPOCH', time.time())),
-    tz=datetime.timezone.utc,
-)
-
-
-# -- Project information -----------------------------------------------------
+_build_time = int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
+_build_date = datetime.datetime.fromtimestamp(_build_time, tz=datetime.timezone.utc)
 
 project = 'meson-python'
-copyright = f'2021\N{EN DASH}{build_date.year} The meson-python developers'
+version = release = mesonpy.__version__
+copyright = f'2021\N{EN DASH}{_build_date.year} The meson-python developers'
 
-# The short X.Y version
-version = mesonpy.__version__
-# The full version, including alpha/beta/rc tags
-release = mesonpy.__version__
+html_theme = 'furo'
+html_title = f'meson-python {version}'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    'sphinx.ext.todo',
     'sphinx_copybutton',
     'sphinx_design',
     'sphinxext.opengraph',
     'sphinx.ext.intersphinx',
 ]
 
-
-try:
-    import sphinxcontrib.spelling
-    extensions.append('sphinxcontrib.spelling')
-    spelling_show_suggestions = True
-    spelling_warning = True
-except ImportError:
-    pass
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-default_role = 'any'
-
-todo_include_todos = True
-
+# sphinx.ext.intersphinx
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'furo'
-html_title = f'meson-python {version}'
-
-html_static_path = ['static']
-html_css_files = [
-    'css/contributors.css',
-]
-
-html_theme_options = {
-    'light_css_variables': {
-        'font-stack': (
-            'system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,'
-            'Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji'
-        ),
-    },
-}
-
-
-# Open Graph
-
+# sphinxext.opengraph
 ogp_site_url = 'https://mesonbuild.com/meson-python/'
 ogp_site_name = 'meson-python documentation'
-
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-# html_static_path = ['_static']
