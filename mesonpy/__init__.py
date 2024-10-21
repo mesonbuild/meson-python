dc69632369ece9e7e10caed137d71e8dc7592cb4
Ralf Gommers <ralf.gommers@gmail.com>
2024-10-21 16:17:50 +0200

    MAINT: set version to 0.18.0.dev0

--- mesonpy/__init__.py
+++ mesonpy/__init__.py
@@ -65,7 +65,7 @@ if typing.TYPE_CHECKING:  # pragma: no cover
     MesonArgs = Mapping[MesonArgsKeys, List[str]]
 
 
-__version__ = '0.17.0.dev0'
+__version__ = '0.18.0.dev0'
 
 
 _NINJA_REQUIRED_VERSION = '1.8.2'
