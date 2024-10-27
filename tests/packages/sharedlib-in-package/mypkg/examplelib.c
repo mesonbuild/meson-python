75bea0e6b73c1adaaca8a069db221e035ec2f670
Ralf Gommers <ralf.gommers@gmail.com>
2024-10-27 18:40:55 +0100

    TST: add test package with internal shared libraries, installed in site-packages

---
+++ tests/packages/sharedlib-in-package/mypkg/examplelib.c
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 The meson-python developers
+//
+// SPDX-License-Identifier: MIT
+
+#include "sub/mypkg_dll.h"
+
+MYPKG_DLL int sum(int a, int b) {
+    return a + b;
+}
