177e7ba425d15cb2d661696d0a6b8ab99296fbe4
Ralf Gommers <ralf.gommers@gmail.com>
2024-10-28 18:02:11 +0100

    TST: add package linking a library from a subproject
    Note that for Meson versions older than 1.2.0, CI failed with:
    ```
    mesonpy.BuildError: Could not map installation path to an equivalent wheel directory: '{libdir_static}/libexamplelib.a'
    ```
    because the `--skip-subprojects` install option isn't honored.
    Hence the test skip on older versions.
    
    In addition, the `c_shared_libs` usage requires Meson 1.3.0

---
+++ tests/packages/link-library-in-subproject/subprojects/bar/examplelib.c
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 The meson-python developers
+//
+// SPDX-License-Identifier: MIT
+
+#include "bar_dll.h"
+
+BAR_DLL int sum(int a, int b) {
+    return a + b;
+}
---
+++ tests/packages/link-library-in-subproject/subprojects/bar/examplelib.h
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2022 The meson-python developers
+//
+// SPDX-License-Identifier: MIT
+
+#include "bar_dll.h"
+
+BAR_DLL int sum(int a, int b);
