1e2e1f203cc7ecc11c575e2fc2f72efd17054c44
Ralf Gommers <ralf.gommers@gmail.com>
2026-01-13 22:52:11 +0100

    TST: change one test package to use `PyModEXPORT_FUNC` (PEP 793)

--- tests/packages/link-library-in-subproject/foo/_examplemod.c
+++ tests/packages/link-library-in-subproject/foo/_examplemod.c
@@ -23,7 +23,23 @@ static PyMethodDef methods[] = {
     {NULL, NULL, 0, NULL},
 };
 
-static struct PyModuleDef module = {
+#if PY_VERSION_HEX >= 0x030F0000
+/* Use `PyModExport_<modname>` hook, new in Python 3.15 (PEP 793) */
+static PyModuleDef_Slot example_slots[] = {
+    {Py_mod_name, "_example"},
+    {Py_mod_methods, methods},
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
+    {0, NULL}
+};
+
+PyMODEXPORT_FUNC
+PyModExport__example(void)
+{
+    return example_slots;
+}
+#else
+/* Use legacy method to create a module dynamically */
+static struct PyModuleDef moduledef = {
     PyModuleDef_HEAD_INIT,
     "_example",
     NULL,
@@ -33,5 +49,17 @@ static struct PyModuleDef module = {
 
 PyMODINIT_FUNC PyInit__example(void)
 {
-    return PyModule_Create(&module);
+    PyObject *module;
+
+    module = PyModule_Create(&moduledef);
+    if (module == NULL) {
+        return module;
+    }
+
+#if Py_GIL_DISABLED
+    PyUnstable_Module_SetGIL(module, Py_MOD_GIL_NOT_USED);
+#endif
+
+    return module;
 }
+#endif
