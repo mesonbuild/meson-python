8da340ff60ab9e96c5061fd7ee8f6eaef2633756
Ralf Gommers <ralf.gommers@gmail.com>
2026-04-29 22:19:33 +0200

    TST: add a test package with dynamic version git hash storing
    Adapted from how this is done in NumPy

---
+++ tests/packages/dynamic-version-from-script/dynamic_version_from_script/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 The meson-python developers
+#
+# SPDX-License-Identifier: MIT
+
+from dynamic_version_from_script._version import __git_hash__, __version__
+
+
+__all__ = ['__git_hash__', '__version__']
