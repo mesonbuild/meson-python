4977b7d1a3b384732f2bbf9ba785fceda75fd0d7
Ralf Gommers <ralf.gommers@gmail.com>
2026-06-09 22:18:00 +0200

    MAINT: set version to 0.20.1.dev0

--- mesonpy/__init__.py
+++ mesonpy/__init__.py
@@ -84,7 +84,7 @@ if typing.TYPE_CHECKING:  # pragma: no cover
     MesonArgs = Mapping[MesonArgsKeys, List[str]]
 
 
-__version__ = '0.20.0'
+__version__ = '0.21.0.dev0'
 
 
 _PYPROJECT_METADATA_VERSION = tuple(map(int, pyproject_metadata.__version__.split('.')[:2]))
